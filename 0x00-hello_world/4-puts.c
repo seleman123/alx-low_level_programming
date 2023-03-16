39d7654e37cf8c637699fdb86f7cb689cb275e79
seleman123 <selemaniradukunda00@gmail.com>
2023-03-16 08:04:04 -0700

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+  * "Programming is like building a multilingual puzzle
+  *followed by a new line
+  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
