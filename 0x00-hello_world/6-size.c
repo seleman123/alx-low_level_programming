e9ec0842d11c8ab15bdec077332c5aeffff088b3
seleman123 <selemaniradukunda00@gmail.com>
2023-03-16 22:01:11 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,7 @@
  * main - prints the size of various types on
  * the computer it is compiled and run on.
  * Return: Always 0.
+ */
 int main(void)
 {
 	printf("size of a char: %zu byte(s)\n", sizeof(char));
