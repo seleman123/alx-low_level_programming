bee087b6294c571e4a003e925b4057ebd0171691
seleman123 <selemaniradukunda00@gmail.com>
2023-03-19 21:02:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ * Return: Always 1 (Sucess)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"
-			- Dora Korpar, 2015-10-19\n");
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, str, sizeof(str) - 1);
 	return (1);
 }
