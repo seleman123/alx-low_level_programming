1caaac3995f7c3e0f47488dd2d3fac2c04d6b923
seleman123 <selemaniradukunda00@gmail.com>
2023-03-27 20:50:38 -0700

    All tasks

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,29 +3,52 @@
 #include <time.h>
 
 /**
- * main - generate rnandom password
- *Description: generate rnandom password
- * Return: A integer
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-int counter = 0;
-srandom(time(NULL));
-char randChar;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-int  passwordLength;
+	srand(time(0));
 
-printf("Type in a password Length \n");
-scanf("%d", &passwordLength);
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
 
-while (counter < passwordLength)
-{
-randChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZa
-	bcdefghijklmnopqrstuvwxyz0123456789"[random() % 62];
-printf("%c", randChar);
-counter++;
-}
-printf("\n");
-return (0);
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+
+	printf("%s", password);
+
+	return (0);
 }
