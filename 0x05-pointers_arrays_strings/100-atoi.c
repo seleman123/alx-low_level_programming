8ba2905ac599d78512780a5e12d1946bb9b0c9ea
seleman123 <selemaniradukunda00@gmail.com>
2023-03-27 20:07:10 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,22 +20,18 @@ int _atoi(char *s)
 	{
 	min *= -1;
 	}
-	
 	while (s[c] >= 48 && s[c] <= 57)
 	{
 	isi = 1;
 	ni = (ni * 10) + (s[c] - '0');
 	c++;
 	}
-	
 	if (isi == 1)
 	{
 	break;
 	}
-	
 	c++;
 	}
-	
 	ni *= min;
 	return (ni);
 }
