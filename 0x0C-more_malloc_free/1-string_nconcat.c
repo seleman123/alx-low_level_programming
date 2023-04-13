b53d4b2eeb70cefd92508fa5a435e381d0e8e24b
seleman123 <selemaniradukunda00@gmail.com>
2023-04-13 03:25:33 -0700

    1 commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
@@ -9,44 +9,43 @@
   * Description: concatenates two strings
   * Return: if the function fails, it returns NULL
   */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0, k = 0, l = 0;
 	char *str;
 
 	if (s1 == NULL)
-		s1 = "";
+	s1 = "";
 	if (s2 == NULL)
-		s2 = "";
+	s2 = "";
 
 	while (s1[i])
-		i++;
+	i++;
 
 	while (s2[k])
-		k++;
+	k++;
 
 	if (n >= k)
-		l = i + k;
+	l = i + k;
 	else
-		l = i + n;
+	l = i + n;
 
 	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
-		return (NULL);
+	return (NULL);
 
 	k = 0;
 	while (j < l)
 	{
-		if (j <= i)
-			str[j] = s1[j];
+	if (j <= i)
+	str[j] = s1[j];
 
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
+	if (j >= i)
+	{
+	str[j] = s2[k];
+	k++;
+	}
+	j++;
 	}
 	str[j] = '\0';
 	return (str);
