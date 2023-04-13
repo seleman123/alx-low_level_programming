d9fb1047bb98b30c3a972d722a6c2171f3d0f033
seleman123 <selemaniradukunda00@gmail.com>
2023-04-13 04:38:08 -0700

    5 commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+
 /**
  * main - multiplies two positive numbers.
  * @argc: number of arguments
@@ -8,45 +8,21 @@
  * Description: multiplies two positive numbers.
  * Return: Nothing
  */
-
-int is_valid_number(char *num)
+int main(int argc, char **argv)
 {
-	while (*num != '\0')
-	{
-		if (!isdigit(*num))
-		{
-			return (0);
-		}
-		num++;
-	}
-	return (1);
+int num1, num2, result;
+
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
 }
-
-int multiply(char *num1, char *num2)
+else
 {
-	return (atoi(num1) * atoi(num2));
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
+printf("%d\n", result);
+return (0);
 }
-
-int main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		fprintf(stderr, "Error\n");
-		exit(98);
-	}
-
-	char *num1 = argv[1];
-	char *num2 = argv[2];
-
-	if (!is_valid_number(num1) || !is_valid_number(num2))
-	{
-		fprintf(stderr, "Error\n");
-		exit(98);
-	}
-
-	int result = multiply(num1, num2);
-
-	printf("%d\n", result);
-
-	return (0);
 }
