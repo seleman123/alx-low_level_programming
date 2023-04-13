8fc106be52bd3a7188627fa4817f6f2adf955d39
seleman123 <selemaniradukunda00@gmail.com>
2023-04-13 14:25:29 -0700

    5 commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,29 +1,103 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * main - multiplies two positive numbers.
- * @argc: number of arguments
- * @argv: array of passed arguments
- * Description: multiplies two positive numbers.
- * Return: Nothing
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int main(int argc, char **argv)
-{
-int num1, num2, result;
 
-if (argc != 3)
+int is_digit(char *s)
 {
-printf("Error\n");
-exit(98);
-}
-else
-{
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 *num2;
-printf("%d\n", result);
-return (0);
-}
+	int x = 0;
+
+	while (s[x])
+	{
+		if (s[x] < '0' || s[x] > '9')
+			return (0);
+		x++;
+	}
+	return (1);
 }
 
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
+
+/**
+ * errors - handles errors for main
+ */
+
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Suiccess)
+ */
+
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, x, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (x = 0; x <= len1 + len2; x++)
+		result[x] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (x = 0; x < len - 1; x++)
+	{
+		if (result[x])
+			a = 1;
+		if (a)
+			putchar(result[x] + '0');
+	}
+	if (!a)
+		putchar('0');
+	putchar('\n');
+	free(result);
+	return (0);
+}
