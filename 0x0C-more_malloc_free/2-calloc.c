4167193b785a16ced88042b69bd17dbf7e067eee
seleman123 <selemaniradukunda00@gmail.com>
2023-04-13 03:31:36 -0700

    2 commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,32 +1,32 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
-  * _calloc - ...
+  * _calloc - allocates memory for an array, using malloc
   * @nmemb: number of members
   * @size: size
-  *
-  * Return: ...
+  * Description:  allocates memory for an array, using malloc
+  * Return: If malloc fails, then _calloc returns NULL
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int i = 0, l = 0;
-	char *p;
+int i = 0, l = 0;
+char *k;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+if (nmemb == 0 || size == 0)
+return (NULL);
 
-	l = nmemb * size;
-	p = malloc(l);
+l = nmemb * size;
+k = malloc(l);
 
-	if (p == NULL)
-		return (NULL);
+if (k == NULL)
+return (NULL);
 
-	while (i < l)
-	{
-		p[i] = 0;
-		i++;
-	}
+while (i < l)
+{
+k[i] = 0;
+i++;
+}
 
-	return (p);
+return (k);
 }
