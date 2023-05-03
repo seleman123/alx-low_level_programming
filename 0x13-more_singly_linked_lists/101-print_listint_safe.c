19a7b174ceeb20bf5d02a93884b877ab5d3cf3e0
seleman123 <selemaniradukunda00@gmail.com>
2023-05-03 12:23:02 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,43 +7,24 @@
  * Return: the number of nodes in the list
  */
 
-size_t looped_listint_len(const listint_t *head)
+size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tortoise, *hare;
-	size_t nodes = 1;
+	size_t num = 0;
+	long int diff;
 
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	tortoise = head->next;
-	hare = (head->next)->next;
-
-	while (hare)
+	while (head)
 	{
-		if (tortoise == hare)
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
 		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
-
-			return (nodes);
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
 		}
-
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
 	}
 
-	return (0);
+	return (num);
 }
-
